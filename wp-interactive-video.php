ca7a74dc914fddf74a8fd328ad7408d18853156a
Marc Chéhab <marcchehab@protonmail.com>
2020-05-31 18:38:30 +0200

    back to normal

--- wp-interactive-video.php
+++ wp-interactive-video.php
@@ -8,7 +8,7 @@ Author URI : https://twitter.com/marcchehab
 */
 
 // ----------------------------------------------------------------------------------
-// Setup + Enqueue Es funktioniert!
+// Setup + Enqueue
 // ----------------------------------------------------------------------------------
 
 define('IV_PLUGIN_URL', __FILE__);
